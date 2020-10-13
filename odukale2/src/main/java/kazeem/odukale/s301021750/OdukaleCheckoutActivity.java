36dca3aba4d97c040b6132716fefd7f115bfb472
kodukale <kodukale.my.centennialcollege.ca>
2020-10-13 17:38:07 -0400

    completed Checkout activity java

--- odukale2/src/main/java/kazeem/odukale/s301021750/OdukaleCheckoutActivity.java
+++ odukale2/src/main/java/kazeem/odukale/s301021750/OdukaleCheckoutActivity.java
@@ -3,9 +3,11 @@ package kazeem.odukale.s301021750;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ViewUtils;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,6 +18,8 @@ public class OdukaleCheckoutActivity extends AppCompatActivity {
 
     ArrayList<String> recievedIntent;
     RadioGroup radioGroup;
+    Button nextCheckoutButton;
+    ArrayList<String> checkoutDetails = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +28,43 @@ public class OdukaleCheckoutActivity extends AppCompatActivity {
         recievedIntent = getIntent().getStringArrayListExtra("selectedApt");
         radioGroup = findViewById(R.id.odukaleRadioGroup);
         getApartmentType(recievedIntent);
+
+        //--- Payment RadioButton---
+        RadioGroup radioGroupPayment = (RadioGroup) findViewById(R.id.odukalePaymentOptionRG);
+        radioGroupPayment.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rCC = (RadioButton) findViewById(R.id.odukaleCreditCardRbtn);
+                RadioButton rbDC = findViewById(R.id.odukaleDebitCardRbtn);
+                if (rCC.isChecked()) {
+                    checkoutDetails.add("Credit Card");
+                } else if (rbDC.isChecked()) {
+                    checkoutDetails.add("Debit Card");
+                }
+                else { }
+            }
+        });
+
+        //--- Apartment option RadioButton---
+        final RadioGroup radioGroupSelectedApt = findViewById(R.id.odukaleRadioGroup);
+        radioGroupSelectedApt.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedApartment = findViewById(checkedId);
+                checkoutDetails.add(selectedApartment.getText().toString());
+            }
+        });
+
+        nextCheckoutButton = findViewById(R.id.odukaleCheckoutButton);
+
+        nextCheckoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OdukaleCheckoutActivity.this, OdukalePaymentActivity.class);
+                intent.putExtra("checkoutKey", checkoutDetails);
+                startActivity(intent);
+            }
+        });
     }
 
     private void getApartmentType(ArrayList<String> dataReceived) {
---
+++ odukale2/src/main/java/kazeem/odukale/s301021750/OdukalePaymentActivity.java
@@ -0,0 +1,14 @@
+package kazeem.odukale.s301021750;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OdukalePaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_odukale_payment);
+    }
+}
